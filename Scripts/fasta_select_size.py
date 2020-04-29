296cbbc95736b7eab171c31dd792e25cb853f899
Gisle Vestergaard <gisle@bio.ku.dk>
2020-04-29 14:25:43 +0200

    Update fasta_select_size.py

--- Scripts/fasta_select_size.py
+++ Scripts/fasta_select_size.py
@@ -9,9 +9,9 @@ parser = argparse.ArgumentParser(description='Select fasta entries by lenght')
 parser.add_argument("-f", "--file", required = True, 
 					help="Input fasta file")
 parser.add_argument("-L", "--large",  
-					help="Select larger or equal to this")
+					help="Select larger or equal to this number of basepairs", type=int)
 parser.add_argument("-S", "--small",  
-					help="Select smaller or equal to this")
+					help="Select smaller or equal to this number of basepairs", type=int)
 parser.add_argument("-o", "--output", required = True, 
 					help="Output fasta file")
 parser.parse_args()
@@ -21,13 +21,16 @@ if args.large is None and args.small is None:
 	parser.print_help()
 
 if args.large:
+	long_sequences = []
 	with open(args.file, "r") as handle:
 		for record in SeqIO.parse(handle, "fasta") :
-			if len(record) >= 1500:
-				print(record.id, len(record))
+			if len(record) >= args.large:
+				long_sequences.append(record)				
+	SeqIO.write(long_sequences, args.output, "fasta")
 if args.small:
+	short_sequences = []
 	with open(args.file, "r") as handle:
 		for record in SeqIO.parse(handle, "fasta") :
-			if len(record) <= 1500:
-				print(record.id, len(record))
-				
+			if len(record) <= args.small:
+				short_sequences.append(record)
+	SeqIO.write(short_sequences, args.output, "fasta")
