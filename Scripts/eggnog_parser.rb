98d6541299c4eb9edda0904301f8966be42bd340
gisleDK <gisves@dtu.dk>
2021-03-09 11:29:35 +0100

    Added Kegg maps summary

--- Scripts/eggnog_parser.rb
+++ Scripts/eggnog_parser.rb
@@ -37,116 +37,153 @@ OptionParser.new do |opts|
     options[:kegg_file] = o
   end
 
-  	opts.on('-u', '--unique', "if r1 hit == r2 hit count them as 1") do |o|
+    opts.on('-p', '--output_kegg_pathways_file <file>', String, 'output kegg pathways file (default is keggpath.collapse)') do |o|
+    options[:keggpath_file] = o
+  end
+    
+    opts.on('-u', '--unique', "if r1 hit == r2 hit count them as 1") do |o|
     options[:uniq] = o
   end
 
 end.parse!
 
-options[:seed_file] ||= 'seed.collapse'
-options[:cat_file] ||= 'cogcat.collapse'
-options[:kegg_file] ||= 'kegg.collapse'
-files			= []
-counts			= {}
-seeds			= []
-cogcats			= []
-cogcat_counts	= Hash.new(0)
-keggs			= []
-kegg_counts	= Hash.new(0)
-samplenames		= []
+options[:seed_file]     ||= 'seed.collapse'
+options[:cat_file]      ||= 'cogcat.collapse'
+options[:kegg_file]     ||= 'kegg.collapse'
+options[:keggpath_file] ||= 'keggpath.collapse'
+files		    	   = []
+counts		  	   = {}
+seeds		     	   = []
+cogcats			     = []
+cogcat_counts	   = Hash.new(0)
+keggs		     	   = []
+kegg_counts	     = Hash.new(0)
+keggpaths        = []
+keggpath_counts = Hash.new(0)
+samplenames		   = []
 
 # Make an array of all input samples
 Dir.glob(options[:input_dir] + '/*emapper.annotations') do |item|
   files << item
 end
-
 # Use Bash to sum counts for each sample of seeds and cog categories
 files.each do |file|
   count       	= []
   seedhash    	= {}
   cogcathash  	= {}
   cogcatcount 	= {}
-  kegghash  	= {}
-  keggcount 	= {}
+  kegghash  	  = {}
+  keggcount 	  = {}
+  keggpathhash  = {}
+  keggpathcount = {}
   if options[:uniq]
-  	sumseeds    = `cat #{file} | grep -v '^#' | cut -f 1,2 | sort -u | cut -f 2 | sort | uniq -c`
-  	sumcogcats  = `cat #{file} | grep -v '^#' | cut -f 1,10 | sort -u | cut -f 2 | sed -e $'s/, /\\\n/g' | sed '/^$/d' | sort | uniq -c`
-  	sumkeggs	= `cat #{file} | grep -v '^#' | cut -f 1,15 | sort -u | cut -f 2 | sed -e $'s/ko://g' | sed -e $'s/,/\\\n/g' | sort | uniq -c | sed 's/-/nil/g'`
+  	sumseeds = `cat #{file} | grep -v '^#' | cut -f 1,2 | sort -u | cut -f 2 | sort | uniq -c`
+  	sumcogcats = `cat #{file} | grep -v '^#' | cut -f 1,10 | sort -u | cut -f 2 | sed -e $'s/, /\\\n/g' | sed '/^$/d' | sort | uniq -c`
+  	sumkeggs = `cat #{file} | grep -v '^#' | cut -f 1,15 | sort -u | cut -f 2 | sed -e $'s/ko://g' | sed -e $'s/,/\\\n/g' | sort | uniq -c | sed 's/-$/nil/g'`
+    sumkeggpaths = `cat #{file} | grep -v '^#' | cut -f 1,16 | sort -u | cut -f 2 | sed -e $'s/,/\\\n/g' | sort | uniq -c | sed 's/-$/nil/g'`
   else
-  	sumseeds    = `cat #{file} | grep -v '^#' | cut -f 2 | sort | uniq -c`
-  	sumcogcats  = `cat #{file} | grep -v '^#' | cut -f 10 | sed '/^$/d' | sed -e $'s/, /\\\n/g' | sort | uniq -c`
-  	sumkeggs	= `cat #{file} | grep -v '^#' | cut -f 15 | sed 's/ko://g' | sed -e $'s/,/\\\n/g' | sort | uniq -c | sed 's/-/nil/g'`
-end
+  	sumseeds = `cat #{file} | grep -v '^#' | cut -f 2 | sort | uniq -c`
+  	sumcogcats = `cat #{file} | grep -v '^#' | cut -f 10 | sed '/^$/d' | sed -e $'s/, /\\\n/g' | sort | uniq -c`
+  	sumkeggs = `cat #{file} | grep -v '^#' | cut -f 15 | sed 's/ko://g' | sed -e $'s/,/\\\n/g' | sort | uniq -c | sed 's/-$/nil/g'`
+    sumkeggpaths = `cat #{file} | grep -v '^#' | cut -f 16 | sed -e $'s/,/\\\n/g' | sort | uniq -c | sed 's/-$/nil/g'`
+  end
 # Make an array of the samplenames
-samplename  = file.split('/').last.split('.').first
-samplenames << samplename
+  samplename  = file.split('/').last.split('.').first
+  samplenames << samplename
 # Make an array of the seeds and make a hash of hashes: samplenames -> seeds -> counts
-sumseeds.each_line do |line|
-	line.chomp!
-	fields = line.split("\t")
-	count = fields[0].split(' ')[0]
-    seeds << fields[0].split(' ')[1]
-    seed = fields[0].split(' ')[1]
-    seedhash[seed] = count
-    counts[samplename] = seedhash
-end
+  sumseeds.each_line do |line|
+	 line.chomp!
+	 fields = line.split("\t")
+	 count = fields[0].split(' ')[0]
+      seeds << fields[0].split(' ')[1]
+      seed = fields[0].split(' ')[1]
+      seedhash[seed] = count
+      counts[samplename] = seedhash
+  end
  # Make an array of the cogcats and make a hash of hashes: samplenames -> cogcats -> counts
-sumcogcats.each_line do |line|
-    line.chomp!
-    fields = line.split("\t")
-    count = fields[0].split(' ')[0]
-    cogcats << fields[0].split(' ')[1].split('')
-    cogcat = fields[0].split(' ')[1].split('')  	
-    cogcat.each do
-    	cogcathash[cogcat] = count
-    end
-end
-cogcats.flatten.uniq.each do |uniq|
-	cogcathash.each do |key, value|
-    value.to_i
-    	key.each do |k|
-    		if k == uniq
-        		if cogcatcount.key?(uniq)
-            		cogcatcount[uniq] += value.to_i
-          		else
-            		cogcatcount[uniq] = value.to_i
-          		end
+  sumcogcats.each_line do |line|
+      line.chomp!
+      fields = line.split("\t")
+      count = fields[0].split(' ')[0]
+      cogcats << fields[0].split(' ')[1].split('')
+      cogcat = fields[0].split(' ')[1].split('')  	
+      cogcat.each do
+    	 cogcathash[cogcat] = count
+      end
+  end
+  cogcats.flatten.uniq.each do |uniq|
+	  cogcathash.each do |key, value|
+      value.to_i
+    	  key.each do |k|
+    		  if k == uniq
+        	  if cogcatcount.key?(uniq)
+              cogcatcount[uniq] += value.to_i
+          	else
+            	cogcatcount[uniq] = value.to_i
+          	end
         	end
-    	end
+    	  end
+      end
     end
-end
-cogcat_counts[samplename] = cogcatcount
+  cogcat_counts[samplename] = cogcatcount
 # Make an array of the kegg orthologs and make a hash of hashes: samplenames -> KO -> counts
-sumkeggs.each_line do |line|
-	line.chomp!
-    fields = line.split("\t")
-    count = fields[0].split(' ')[0]
-    keggs << fields[0].split(' ')[1].split(' ')
-    kegg = fields[0].split(' ')[1].split(' ')
-    kegg.each do
-    	kegghash[kegg] = count
+  sumkeggs.each_line do |line|
+	  line.chomp!
+      fields = line.split("\t")
+      count = fields[0].split(' ')[0]
+      keggs << fields[0].split(' ')[1].split(' ')
+      kegg = fields[0].split(' ')[1].split(' ')
+      kegg.each do
+    	  kegghash[kegg] = count
+      end
+  end
+  keggs.flatten.uniq.each do |uniq|
+	  kegghash.each do |key, value|
+    value.to_i
+      key.each do |k|
+       	if k == uniq
+         	if keggcount.key?(uniq)
+          	keggcount[uniq] += value.to_i
+          else
+          	keggcount[uniq] = value.to_i
+          end
+        end
+      end
     end
-end
-keggs.flatten.uniq.each do |uniq|
-	kegghash.each do |key, value|
-    	value.to_i
-      		key.each do |k|
-        	if k == uniq
-         		if keggcount.key?(uniq)
-            		keggcount[uniq] += value.to_i
-          		else
-            		keggcount[uniq] = value.to_i
-          		end
-        	end
-      	end
-    end
-end
+  end
 kegg_counts[samplename] = keggcount
+# Make an array of the kegg pathways and make a hash of hashes: samplenames -> KEGG maps -> counts
+  sumkeggpaths.each_line do |line|
+    line.chomp!
+      fields = line.split("\t")
+      count = fields[0].split(' ')[0]
+      keggpaths << fields[0].split(' ')[1].split(' ')
+      keggpath = fields[0].split(' ')[1].split(' ')
+      keggpath.each do
+        keggpathhash[keggpath] = count
+      end
+  end
+  keggpaths.flatten.uniq.each do |uniq|
+    keggpathhash.each do |key, value|
+    value.to_i
+      key.each do |k|
+        if k == uniq
+          if keggpathcount.key?(uniq)
+            keggpathcount[uniq] += value.to_i
+          else
+            keggpathcount[uniq] = value.to_i
+          end
+        end
+      end
+    end
+  end
+keggpath_counts[samplename] = keggpathcount
 end
 
 seeds.uniq!
 cogcats.flatten!.uniq!
 keggs.flatten!.uniq!
+keggpaths.flatten!.uniq!
 
 # Write seed summary
 File.open(options[:seed_file], 'w') do |file|
@@ -186,6 +223,23 @@ File.open(options[:kegg_file], 'w') do |file|
   kegg_counts.each do |sample, data|
     file.print(sample)
     keggs.each do |ko|
+      if data.key?(ko)
+        file.print("\t", data[ko])
+      else
+        file.print("\t0")
+      end
+    end
+    file.print("\n")
+  end
+end
+
+
+# Write KEGG pathway summary
+File.open(options[:keggpath_file], 'w') do |file|
+  file.print("\t", keggpaths.join("\t"), "\n")
+  keggpath_counts.each do |sample, data|
+    file.print(sample)
+    keggpaths.each do |ko|
       if data.key?(ko)
         file.print("\t", data[ko])
       else
